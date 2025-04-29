b55abf81b1a9915b9496a56c0ce25fb8dd84c065
Sudhanshu Mishra <115578381+Sudhanshu-m@users.noreply.github.com>
2025-04-29 20:54:58 +0530

    Update app.config.ts

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,19 +1,19 @@
 export type AppConfig = {
-    apiPrefix: string
-    authenticatedEntryPath: string
-    unAuthenticatedEntryPath: string
-    locale: string
-    accessTokenPersistStrategy: 'localStorage' | 'sessionStorage' | 'cookies'
-    enableMock: boolean
+    apiPrefix: string;
+    authenticatedEntryPath: string;
+    unAuthenticatedEntryPath: string;
+    locale: string;
+    accessTokenPersistStrategy: 'localStorage' | 'sessionStorage' | 'cookies';
+    enableMock: boolean;
 }
 
 const appConfig: AppConfig = {
     apiPrefix: '/api',
-    authenticatedEntryPath: '/hello',
-    unAuthenticatedEntryPath: '/hello',
+    authenticatedEntryPath: '/',  // Changed from /hello to /
+    unAuthenticatedEntryPath: '/',  // Changed from /hello to /
     locale: 'en',
     accessTokenPersistStrategy: 'cookies',
     enableMock: true,
 }
 
-export default appConfig
+export default appConfig;
